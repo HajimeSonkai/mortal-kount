c2168332e9ec3c48eb5e800a43cd4d9f486396f2
sonkai <mfilho86@gmail.com>
2021-08-16 19:57:56 -0300

    primeiro contador funcionando

--- src/Components/KountDown.js
+++ src/Components/KountDown.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
 class KountDown extends React.Component {
+    constructor () {
+        super()
+        this.state = {
+            secondsRemaining: 1000,
+        }
+    }
+
+startCountdown = () => {
+    setInterval(() => this.setState((prevState) => ({
+        secondsRemaining:
+        prevState.secondsRemaining - 1
+    })), 1000);
+    if (this.state.secondsRemaining === 0) {
+        clearInterval();
+    }
+}
+
     render(){
         return (
-            <p>Contador</p>
+            <>
+            <p>{ this.state.secondsRemaining }</p>
+            <button onClick = { this.startCountdown }>Kount</button>
+            </>
         );
     }
 }
