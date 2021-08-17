4c7445f97f2effb4e6c4919eaa63f5c819a79204
sonkai <mfilho86@gmail.com>
2021-08-16 21:05:30 -0300

    implementei o imput de tempo

--- src/Components/KountDown.js
+++ src/Components/KountDown.js
@@ -3,33 +3,58 @@ import TimeInput from './TimeImput';
 import TimeDisplay from './TimeDisplay';
 
 class KountDown extends React.Component {
-    constructor () {
-        super()
-        this.state = {
-            secondsRemaining: 10,
-        }
-    }
-
-startCountdown = () => {
-    setInterval(() => this.setState((prevState) => ({
-        secondsRemaining:
-        prevState.secondsRemaining - 1
-    })), 1000);
-    if (this.state.secondsRemaining === 0) {
-        clearInterval();
-    }
+  constructor () {
+    super()
+      this.state = {
+        secondsRemaining: 0,
+        inputHours: '',
+        inputMinuts: '',
+        inputSeconds: '',
+  }
 }
 
-    render(){
-        const {secondsRemaining} = this.state
-        return (
-            <>
-            <TimeInput />
-            <TimeDisplay secondsRemaining = { secondsRemaining } />
-            <button onClick = { this.startCountdown }>Kount</button>
-            </>
-        );
+startCountdown = () => {
+  const updateState = () => {
+    if (this.state.secondsRemaining > 0){
+      this.setState ((prevState) => ({ secondsRemaining: prevState.secondsRemaining -1 }))
+    } else {
+      clearInterval(updateState);
     }
+  }
+  setInterval(updateState, 1000);
+}
+
+getTimeFromImput = ({ target }) => {
+  const { name } = target;
+  let { value } = target;
+  if (value === '') value = 0;
+
+  this.setState ({[name]: parseFloat(value)}, () => {
+    this.setState({
+      secondsRemaining: (
+        this.state.inputHours*3600 +
+        this.state.inputMinuts*60 +
+        this.state.inputSeconds
+      )
+    })
+  })
+}
+
+render(){
+  const { secondsRemaining, inputHours, inputMinuts, inputSeconds } = this.state
+  return (
+    <>
+      <TimeInput
+      updateTimer = {this.getTimeFromImput }
+      inputHours = { inputHours }
+      inputMinuts = { inputMinuts }
+      inputSeconds = { inputSeconds }
+      />
+      <TimeDisplay secondsRemaining = { secondsRemaining } />
+      <button onClick = { this.startCountdown }>Kount</button>
+    </>
+    );
+  }
 }
 
 export default KountDown;
--- src/Components/TimeImput.js
+++ src/Components/TimeImput.js
@@ -2,16 +2,28 @@ import React from 'react'
 
 class TimeInput extends React.Component {
   render() {
+    const { updateTimer, inputHours, inputMinuts, inputSeconds } = this.props;
     return (
       <>
         <label>Hours
-          <input type="number" />
+          <input type="number"
+          name = "inputHours"
+          onChange = { updateTimer }
+          value = { inputHours }
+          />
         </label>
         <label>Minutes
-          <input type="number" />
+          <input type="number"
+          name = "inputMinuts"
+          onChange = { updateTimer }
+          value = { inputMinuts }
+          />
         </label>
         <label>Seconds
-          <input type="number" />
+          <input type="number"
+          name = "inputSeconds"
+          onChange = { updateTimer }
+          value = { inputSeconds }/>
         </label>
       </>
     )
